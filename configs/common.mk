98efb3115047e100c0c7bc9f87b80db8035e9520
wtogami <wtogami@gmail.com>
2015-03-16 18:13:40 +0500

    Modular backuptool.sh. Executes backup and restore methods

--- configs/common.mk
+++ configs/common.mk
@@ -54,6 +54,12 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/addon.d/71-layers.sh:system/addon.d/71-layers.sh
 endif
 
+# Backuptool support
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/addon.d/50-simpleaosp.sh:system/addon.d/50-simpleaosp.sh \
+    $(LOCAL_PATH)/bin/backuptool.functions:system/bin/backuptool.functions \
+    $(LOCAL_PATH)/bin/backuptool.sh:system/bin/backuptool.sh
+
 # Bootanimation support
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/bootanimation.zip:system/media/bootanimation.zip
