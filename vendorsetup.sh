bd22ba0f3ee973c09c0a7c7df9292c6db7608fc0
snak3ater <saeedyasir91@gmail.com>
2015-12-01 20:51:48 +0500

    Switch to user builds

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo saosp_bullhead-userdebug
-add_lunch_combo saosp_flo-userdebug
-add_lunch_combo saosp_hammerhead-userdebug
-add_lunch_combo saosp_mako-userdebug
+add_lunch_combo saosp_bullhead-user
+add_lunch_combo saosp_flo-user
+add_lunch_combo saosp_hammerhead-user
+add_lunch_combo saosp_mako-user
